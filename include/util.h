1c99193f20ea4a5bad1729eb2a3303074e06d909
kwikrick <rickvandermeiden@gmail.com>
2025-05-30 12:22:42 +0200

    randomization util (expensive!)

--- include/util.h
+++ include/util.h
@@ -7,3 +7,18 @@ float randf(float min, float max) {
   return ((float)rand() * (max-min) / (float)RAND_MAX) + min;
 
 }
+
+
+
+// util
+
+template<typename T> std::vector<T> randomize(std::vector<T> in) {
+    // note: input by value, makes a copy, we can safely erase from
+    std::vector<T> out;
+    while( !in.empty()) {
+        int i = rand() % in.size();
+        out.push_back(in[i]);       // copy
+        in.erase(in.begin() + i);
+    }
+    return out;
+}
